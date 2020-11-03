539eee3fe13465ebe2e3edc63d000bbf1ed1f17f
Eric Osowski <ericosowski@Erics-MacBook-Pro.local>
2020-11-03 12:49:13 -0600

    Villagers now searching by name and returning villager option

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -11,26 +11,43 @@ const { villagers } = require("animal-crossing")
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
 
   // GET route for getting all of the villagers
-  app.get("/api/villagers", function(req, res) {
+  app.get("/api/villagers", function (req, res) {
     db.Villager.findAll()
-      .then(function(dbVillager) {
+      .then(function (dbVillager) {
         res.json(dbVillager);
       });
   });
 
-  app.post("/api/villagers", function(req, res) {
+  app.post("/api/villagers", function (req, res) {
     db.Villager.create(req.body)
-      .then(function() {
+      .then(function () {
         res.status(200).end();
       });
   });
 
-  app.get("/api/all_villagers", function(req, res){
+  app.get("/api/all_villagers", function (req, res) {
 
     res.json(villagers);
+
+  });
+
+  app.get("/api/oneVillager/:villagerName", function (req, res) {
+
+    function findVillagerByName(name) {
+      for (let i = 0; i < villagers.length; i++) {
+        if (villagers[i].name === name) {
+          return villagers[i];
+        }
+      }
+    }
+    let currentVillager = findVillagerByName(req.params.villagerName)
+    console.log(req.params.villagerName);
+    console.log(currentVillager);
+    // console.log("Current villager is: " + currentVillager);
+    res.json(currentVillager);
 
   })
 };
