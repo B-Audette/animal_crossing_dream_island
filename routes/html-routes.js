999d0f88375cba53f860914974616caf1bbdfe02
B-Audette <b.audette015@gmail.com>
2020-11-03 14:12:41 -0600

    renamed index2 to index, index from before has moved to depreciated

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = function(app) {
 
-    app.get("/ki", function (req, res){
+    app.get("/", function (req, res){
         res.sendFile(path.join(__dirname,"../public/index2.html"))
     })
 
