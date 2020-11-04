c3270a3940e58d21cd88edd27ecc8158d5e6e603
B-Audette <b.audette015@gmail.com>
2020-11-03 20:20:32 -0600

    connected html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,4 +6,12 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname,"../public/index.html"))
     })
 
+    app.get("/login", function (req, res){
+        res.sendFile(path.join(__dirname,"../public/login.html"))
+    })
+
+    app.get("/signup", function (req, res){
+        res.sendFile(path.join(__dirname,"../public/signup.html"))
+    })
+
 }
